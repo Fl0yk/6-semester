4caa3e9b5abbf49f3a80857c7a231493acb95451
Дима Косач <kosach.dmitriy@gmail.com>
2024-01-28 01:49:59 +0300

    End isob lab 1 code

--- ИСОБ/ISOB_labs/CaesarСipher(lab_1)/Ciphers.cs
+++ ИСОБ/ISOB_labs/CaesarСipher(lab_1)/Ciphers.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Reflection;
+using System.Text;
 
 namespace CaesarСipher_lab_1_
 {
@@ -7,6 +8,7 @@ namespace CaesarСipher_lab_1_
         private const int _caesarKey = 3;
         private readonly static List<char> _smallLitters = new();
         private readonly static List<char> _largeLitters = new();
+        private readonly static int _alphabetSize;
 
         static Ciphers()
         {
@@ -19,65 +21,103 @@ namespace CaesarСipher_lab_1_
             for (int i = (int)'А'; i <= (int)'Я'; i++)
                 _largeLitters.Add((char)i);
             _largeLitters.Insert(6, 'Ё');
+
+            _alphabetSize = _smallLitters.Count;
         }
 
         public static string CeasarCipherEncrypt(string input)
         {
-            StringBuilder output = new StringBuilder(input.Length);
-            
-            foreach (char c in input)
-            {
-                //Проверяем большие буквы и смещаем
-                int largInd = _largeLitters.IndexOf(c);
-                if(largInd >= 0)
-                {
-                    output.Append(_largeLitters[(largInd + _caesarKey) % _largeLitters.Count]);
-                    continue;
-                }
-
-                //Если в больших буквах не нашли, то ищем в маленьких
-                int smallInd = _smallLitters.IndexOf(c);
-                if(smallInd >= 0)
-                {
-                    output.Append(_smallLitters[(smallInd + _caesarKey) % _smallLitters.Count]);
-                    continue;
-                }
-
-                //Если какой-то символ не из русского алфавита, то просто пихаем на свое место
-                output.Append(c);
-            }
-
-            return output.ToString();
+            return CeasarCipher(input, true);
         }
 
-
         public static string CeasarCipherDecrypt(string input)
+        {
+            return CeasarCipher(input, false);
+        }
+
+        private static string CeasarCipher(string input, bool isEncrypt)
         {
             StringBuilder output = new(input.Length);
 
             foreach (char c in input)
             {
-                //Проверяем большие буквы и смещаем
-                int largInd = _largeLitters.IndexOf(c);
-                if (largInd >= 0)
-                {
-                    output.Append(_largeLitters[(largInd - _caesarKey + _largeLitters.Count) % _largeLitters.Count]);
-                    continue;
-                }
-
-                //Если в больших буквах не нашли, то ищем в маленьких
-                int smallInd = _smallLitters.IndexOf(c);
-                if (smallInd >= 0)
-                {
-                    output.Append(_smallLitters[(smallInd - _caesarKey + _smallLitters.Count) % _smallLitters.Count]);
-                    continue;
-                }
-
-                //Если какой-то символ не из русского алфавита, то просто пихаем на свое место
-                output.Append(c);
+                output.Append(Shift(c, _caesarKey, isEncrypt));
             }
 
             return output.ToString();
+        }
+
+        public static string VigenereCipherEncrypt(string input, string keyWord)
+        {
+            return VigenereCipher(input, keyWord, true);
+        }
+
+        public static string VigenereCipherDecrypt(string input, string keyWord)
+        {
+            return VigenereCipher(input, keyWord, false);
+        }
+
+        private static string VigenereCipher(string input, string keyWord, bool isEncrypt)
+        {
+            StringBuilder output = new(input.Length);
+
+            for(int i = 0, j = 0; i < input.Length; i++)
+            {
+                //Нам нужен индекс буквы из ключевого слова
+                //Так что сначала пытаемся найти среди больших
+                int tryLargeInd = _largeLitters.IndexOf(keyWord[j % keyWord.Length]);
+                //А затем проверяем, то ли мы нашли. Если нет, то ищем маленькую
+                int keyId = tryLargeInd < 0 
+                    ? _smallLitters.IndexOf(keyWord[j % keyWord.Length]) 
+                    : tryLargeInd;
+
+                if (input[i].IsRusLetter())
+                {
+                    //Казалось бы, зачем. А это для того, чтоб кодовое слово не уезжало,
+                    //когда в основной строке пробел, запятая и т.д.
+                    //Мне было бы пофиг, но в калькуляторах не уезжает
+                    j++;
+                }
+
+                output.Append(Shift(input[i], keyId, isEncrypt));
+            }
+
+            return output.ToString();
+        }
+
+        private static char Shift(char letter, int key, bool isEncrypt)
+        {
+            //Проверяем большие буквы и возвращаем смещенную букву
+            int largInd = _largeLitters.IndexOf(letter);
+            if (largInd >= 0)
+            {
+                return _largeLitters[(_alphabetSize + largInd + key * (isEncrypt ? 1 : -1)) % _alphabetSize];
+            }
+
+            //Если в больших буквах не нашли, то ищем в маленьких
+            int smallInd = _smallLitters.IndexOf(letter);
+            if (smallInd >= 0)
+            {
+                return _smallLitters[(_alphabetSize + smallInd + key * (isEncrypt ? 1 : -1)) % _alphabetSize];
+            }
+            
+            //Если какой-то символ не из русского алфавита, то просто пихаем на свое место
+            return letter;
+        }
+    }
+
+    public static class CharExtension
+    {
+        /// <summary>
+        /// Проверяет, является ли символ буквой русского алфавита
+        /// </summary>
+        /// <param name="letter">Потенциальная русская буква</param>
+        /// <returns></returns>
+        public static bool IsRusLetter(this char letter)
+        {
+            return (char.IsBetween(letter, 'а', 'я')
+                    || char.IsBetween(letter, 'А', 'Я')
+                    || letter == 'ё' || letter == 'Ё');
         }
     }
 }
--- ИСОБ/ISOB_labs/CaesarСipher(lab_1)/Program.cs
+++ ИСОБ/ISOB_labs/CaesarСipher(lab_1)/Program.cs
@@ -1,17 +1,50 @@
 ﻿using CaesarСipher_lab_1_;
 
-Console.WriteLine((int)'а');    //1072
-Console.WriteLine((int)'я');    //1103
-Console.WriteLine((int)'ё');    //1105
-Console.WriteLine((int)'А');    //1040
-Console.WriteLine((int)'Я');    //1071
-Console.WriteLine((int)'Ё');    //1025
-Console.WriteLine((char)1004);  //?
-Console.WriteLine("================");
+string fileName = "test.txt";
+
+using (StreamReader sr = new StreamReader(fileName))
+{
+    string text = sr.ReadLine();
+    string keyWord = sr.ReadLine();
+
+    Console.WriteLine($"Считанный текст из файла: {text}");
+    Console.WriteLine($"Считанное кодовое слово: {keyWord}");
+    Console.WriteLine("\n========================================\n");
+
+    string ceaserEncode = Ciphers.CeasarCipherEncrypt(text);
+    Console.WriteLine($"Зашифрованный текст с помощью шифра Цезаря: \n{ceaserEncode}\n");
+    Console.WriteLine($"Расшифрованный текст с помощью шифра Цезаря: \n{Ciphers.CeasarCipherDecrypt(ceaserEncode)}");
+    Console.WriteLine("\n========================================\n");
+
+    string vigenereEncode = Ciphers.VigenereCipherEncrypt(text, keyWord);
+    Console.WriteLine($"Зашифрованный текст с помощью шифра Виженера: \n{vigenereEncode}\n");
+    Console.WriteLine($"Расшифрованный текст с помощью шифра Виженера: \n{Ciphers.VigenereCipherDecrypt(vigenereEncode, keyWord)}");
+    Console.WriteLine("\n========================================\n");
+}
 
 
-string input = "Съешь же ещё этих мягких французских булок, да выпей чаю.";
-string res = Ciphers.CeasarCipherEncrypt(input);
 
-Console.WriteLine(Ciphers.CeasarCipherEncrypt(input));
-Console.WriteLine(Ciphers.CeasarCipherDecrypt(res));
+
+
+//Console.WriteLine((int)'а');    //1072
+//Console.WriteLine((int)'я');    //1103
+//Console.WriteLine((int)'ё');    //1105
+//Console.WriteLine((int)'А');    //1040
+//Console.WriteLine((int)'Я');    //1071
+//Console.WriteLine((int)'Ё');    //1025
+//Console.WriteLine((char)1004);  //?
+//Console.WriteLine("================");
+
+
+//string input = "Съешь же ещё этих мягких французских булок, да выпей чаю.";
+//string key = "Вова";
+//string res = Ciphers.CeasarCipherEncrypt(input);
+
+//Console.WriteLine(Ciphers.CeasarCipherEncrypt(input));
+//Console.WriteLine(Ciphers.CeasarCipherDecrypt(res));
+
+//Console.WriteLine("=================");
+//res = Ciphers.VigenereCipherEncrypt(input, key);
+
+//Console.WriteLine(res);
+//Console.WriteLine(Ciphers.VigenereCipherDecrypt(res, key));
