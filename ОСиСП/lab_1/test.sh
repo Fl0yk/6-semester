f5ea8f48af142abac104570551879412b8750c4f
Fl0yk <kosach.dmitriy@mail.ru>
2024-02-06 00:44:22 +0300

    OSiSP_lab2. Create awk regex

--- ОСиСП/lab_1/test.sh
+++ ОСиСП/lab_1/test.sh
@@ -1,7 +1,53 @@
 val=3
-val2=5
+val2=-3
+sum=$(( $val+$val2 ))
 
-if [$val == 3]
+if [[ $val+$val2 ]]
 then
     echo "Ok"
+    echo "$sum"
+else
+    echo "${val[1]}"
 fi
+
+function is_ok {
+    return 1
+}
+
+is_ok
+
+a=$(( ! true ))
+echo $a
+
+if ! true
+then
+    echo "is ok. Res: $is_ok"
+fi
+
+#Проверяем, есть ли файл и обычный ли он
+#Второе, что файл не пустой
+if [[ -f ./test.txt && -s ./test.txt ]]
+then
+    echo "Est"
+fi
+
+text= cat "test.txt"
+echo "${text[0]}"
+
+function get_data {
+    str=$1
+    name="Abobus"
+    count=12
+    return 0
+}
+
+if get_data
+then
+    echo $name $count
+else
+    echo Err
+    exit
+fi
+echo ================
+patter='^"[A-Za-z ]+" [[:digit:]]+$'
+awk "/$patter/" test.txt
