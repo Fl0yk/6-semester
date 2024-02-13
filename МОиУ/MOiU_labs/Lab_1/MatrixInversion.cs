e22b0f76c5e92162d90d971e44bbc933c0dec37a
Дима Косач <kosach.dmitriy@gmail.com>
2024-02-14 01:43:55 +0300

    MOiU_lab1. End matrix inverse

---
+++ МОиУ/MOiU_labs/Lab_1/MatrixInversion.cs
@@ -0,0 +1,50 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace lab_1
+{
+    public class MatrixInversion
+    {
+        public static Matrix<double> FindInversionMatrix(Matrix<double> a_1, Matrix<double> x, int i)
+        {
+            int size = a_1.RowCount;
+
+            //1
+            var l = a_1 * x;
+
+            if (l[--i, 0] == 0)
+                throw new Exception("Матрица необратима");
+            //2
+            var l_i = l[i, 0];
+            l[i, 0] = -1;
+            //3
+            var l_krisha = (-1 / l_i) * l;
+            //4
+            Matrix<double> q = DenseMatrix.CreateDiagonal(size, size, 1);
+
+            for (int j = 0; j < size; j++)
+            {
+                q[j, i] = l_krisha[j, 0];
+            }
+
+            //5
+            Matrix<double> res = DenseMatrix.Create(size, size, 0);
+
+            for (int j = 0; j < size; j++)
+            {
+                for (int k = 0; k < size; k++)
+                {
+                    if (j == i)
+                    {
+                        res[j, k] = q[j, j] * a_1[k, i];
+                        continue;
+                    }
+
+                    res[j, k] = q[j, j] * a_1[j, k] + q[j, i] * a_1[k, i];
+                }
+            }
+
+            return res;
+        }
+    }
+}
---
+++ МОиУ/MOiU_labs/Lab_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using lab_1;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+Matrix<double> a = DenseMatrix.OfArray(new double[,]{
+    {1, -1, 0},
+    {0, 1, 0},
+    {0, 0, 1 }
+});
+
+Matrix<double> a_1 = DenseMatrix.OfArray(new double[,]{
+    {1, 1, 0},
+    {0, 1, 0},
+    {0, 0, 1 }
+});
+
+Matrix<double> x = DenseMatrix.OfArray(new double[,]
+{
+    {1},
+    {0},
+    {1}
+});
+
+var res = MatrixInversion.FindInversionMatrix(a_1, x, 3);
+Console.WriteLine(res);
