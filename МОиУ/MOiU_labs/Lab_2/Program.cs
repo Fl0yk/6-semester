72ea320b504d50acf69b67c2fa871bae8d34256f
Fl0yk <kosach.dmitriy@mail.ru>
2024-03-10 19:11:11 +0300

    MOiU_lab2. End main phase simplex method

---
+++ МОиУ/MOiU_labs/Lab_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using Lab_2;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+using System.Runtime.ConstrainedExecution;
+
+Matrix<double> a = DenseMatrix.OfArray(new double[,]{
+    {-1, 1, 1, 0, 0},
+    {1, 0, 0, 1, 0},
+    {0, 1, 0, 0, 1 }
+});
+
+Matrix<double> c = DenseVector.OfArray([1, 1, 0, 0, 0]).ToRowMatrix();
+
+Matrix<double> x = DenseVector.OfArray([0, 0, 1, 3, 2]).ToColumnMatrix();
+
+List<int> b = [3, 4, 5];
+
+var res = SimplexMethod.MainPhase(a, c, x, b);
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("\nРезультаты!\n");
+Console.WriteLine(res);
+Console.ResetColor();
